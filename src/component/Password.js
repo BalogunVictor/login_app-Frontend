58b22563ea37b54ccd5e252176a9600b2c96b594
BalogunVictor <balogunmoyinoluwav@gmail.com>
2023-12-18 16:11:33 +0100

    fetch hook

--- src/component/Password.js
+++ src/component/Password.js
@@ -5,8 +5,13 @@ import styles from "../styles/Username.module.css";
 import { Toaster } from "react-hot-toast";
 import { useFormik } from "formik";
 import { passwordValidate } from "../helper/validate";
+import { useAuthStore } from "../store/store";
+import useFetch from "../hooks/fetch.hook";
 
 const Password = () => {
+  const { username } = useAuthStore((state) => state.auth);
+  const [{ isLoading, apiData, serverError }] = useFetch(`/user/${username}`);
+
   const formik = useFormik({
     initialValues: {
       password: "",
@@ -19,6 +24,9 @@ const Password = () => {
     },
   });
 
+  if (isLoading) return <hi className="text-2xl font-bold">isLoading</hi>;
+  if (serverError)
+    return <hi className="text-xl text-red-500">{serverError.message}</hi>;
   return (
     <div className="container mx-auto">
       <Toaster position="top-center"></Toaster>
@@ -26,7 +34,9 @@ const Password = () => {
       <div className="flex justify-center items-center h-screen">
         <div className={styles.glass}>
           <div className="title flex flex-col items-center">
-            <h4 className="text-5xl font-bold">Hello Again!</h4>
+            <h4 className="text-5xl font-bold">
+              Hello {apiData?.firstName || apiData?.username}
+            </h4>
             <span className="py-4 text-xl w-2/3 text-center text-gray-500">
               Explore More by connecting with us.
             </span>
@@ -34,7 +44,11 @@ const Password = () => {
 
           <form className="py-1" onSubmit={formik.handleSubmit}>
             <div className="profile flex justify-center py-4">
-              <img src={avatar} className={styles.profile_img} alt="avatar" />
+              <img
+                src={apiData?.profile || avatar}
+                className={styles.profile_img}
+                alt="avatar"
+              />
             </div>
 
             <div className="textbox flex flex-col items-center gap-6">
