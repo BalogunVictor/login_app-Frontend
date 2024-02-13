310cad9d860e5bad512bc2d3eafd92359a6b1cc5
BalogunVictor <balogunmoyinoluwav@gmail.com>
2024-02-13 16:33:00 +0100

    Recovery Page

--- src/component/Recovery.js
+++ src/component/Recovery.js
@@ -1,9 +1,8 @@
 import React, { useEffect, useState } from "react";
 import toast, { Toaster } from "react-hot-toast";
-import { useAuthStore } from "../store/store";
-import styles from "../styles/Username.module.css";
-import { generateOTP, verifyOTP } from "../helper/helper";
 import { useNavigate } from "react-router-dom";
+import { generateOTP, verifyOTP } from "../helper/helper";
+import { useAuthStore } from "../store/store";
 
 export default function Recovery() {
   const { username } = useAuthStore((state) => state.auth);
@@ -47,47 +46,47 @@ export default function Recovery() {
   }
 
   return (
-    <div className="container mx-auto">
+    <div className="container w-1/2 mx-auto mt-5">
       <Toaster position="top-center" reverseOrder={false}></Toaster>
 
-      <div className="flex justify-center items-center h-screen">
-        <div className={styles.glass}>
-          <div className="title flex flex-col items-center">
-            <h4 className="text-5xl font-bold">Recovery</h4>
-            <span className="py-4 text-xl w-2/3 text-center text-gray-500">
-              Enter OTP to recover password.
-            </span>
-          </div>
+      <div className="title flex flex-col items-center">
+        <h4 className="text-5xl font-bold">Recovery</h4>
+        <span className="py-4 text-xl w-2/3 text-center text-gray-500">
+          Enter OTP to recover password.
+        </span>
+      </div>
 
-          <form className="pt-20" onSubmit={onSubmit}>
-            <div className="textbox flex flex-col items-center gap-6">
-              <div className="input text-center">
-                <span className="py-4 text-sm text-left text-gray-500">
-                  Enter 6 digit OTP sent to your email address.
-                </span>
-                <input
-                  onChange={(e) => setOTP(e.target.value)}
-                  className={styles.textbox}
-                  type="text"
-                  placeholder="OTP"
-                />
-              </div>
-
-              <button className={styles.btn} type="submit">
-                Recover
-              </button>
-            </div>
-          </form>
-
-          <div className="text-center py-4">
-            <span className="text-gray-500">
-              Can't get OTP?{" "}
-              <button onClick={resendOTP} className="text-red-500">
-                Resend
-              </button>
-            </span>
-          </div>
+      <form className="text-xl" onSubmit={onSubmit}>
+        <div className="input text-center">
+          <span className="py-4 text-sm text-left text-gray-500">
+            Enter 6 digit OTP sent to your email address.
+          </span>
         </div>
+
+        <div className="mt-2">
+          <label for="username">OTP</label>
+          <input
+            onChange={(e) => setOTP(e.target.value)}
+            type="text"
+            placeholder="OTP"
+            className="w-full h-8 bg-gray-200 border-solid border-2 border-black rounded block mt-2 tracking-widest pl-2"
+          />
+        </div>
+
+        <div className="mt-10">
+          <button className="btn p-1 bg-gray-800 text-white w-full rounded-full h-12 font-bold outline-none tracking-wide">
+            Recover
+          </button>
+        </div>
+      </form>
+
+      <div className="text-center py-4">
+        <span className="text-gray-500">
+          Can't get OTP?{" "}
+          <button onClick={resendOTP} className="text-red-500">
+            Resend
+          </button>
+        </span>
       </div>
     </div>
   );
